dbdd7281a6f9fdd996722459bdabe9d0e51d8554
Eyosyas Fetene <eyosifetene98@gmail.com>
2024-05-27 12:18:53 +0300

    s

---
+++ src/NewPrinter.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import IsoComponent from "./IsoComponent";
+
+export default function NewPrinter() {
+  return (
+    <div>
+      <IsoComponent />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque,
+        reprehenderit inventore! Praesentium at velit voluptate esse sequi
+        impedit architecto quae, minus beatae vitae facere deleniti mollitia
+        quis autem quidem unde minima dicta hic commodi? Officia magnam, placeat
+        nemo reprehenderit voluptas sint, distinctio nihil aspernatur
+        dignissimos deleniti nesciunt iusto. Atque labore fuga eligendi quae
+        fugit ex aspernatur culpa recusandae, deserunt officia reprehenderit
+        tenetur voluptates temporibus quos illo molestias ipsum iusto placeat.
+        Voluptate veniam dolore voluptates, cumque assumenda dolorem quod, atque
+        ab velit aliquam illum labore corrupti sint tenetur quam hic itaque
+        voluptas neque magni deserunt consectetur exercitationem! Quod sapiente
+        repellendus quae id ratione odio, dolor nostrum similique reprehenderit
+        nam pariatur dolorem explicabo voluptatibus eum itaque laborum tempore,
+        expedita obcaecati veritatis? Illo fugit aliquid est tenetur itaque
+        exercitationem, veniam repudiandae suscipit dolores maiores harum omnis
+        eum dolor natus ipsam ea, saepe neque modi. Vitae iure cum maxime
+        placeat doloremque? Voluptas quaerat odit ex nobis obcaecati architecto,
+        debitis nesciunt facere omnis beatae voluptate unde dolorum repudiandae
+        molestiae quam adipisci dolorem nam modi consectetur et cupiditate
+        labore magnam temporibus? Nostrum esse culpa quidem laboriosam ullam
+        dolorum dolores mollitia ipsa, dignissimos aspernatur omnis accusamus
+        sit possimus doloremque, delectus minima sapiente. Accusamus molestias,
+        distinctio molestiae ad necessitatibus repellat officia quaerat ut
+        deleniti. Eveniet blanditiis consequuntur atque eligendi repellat
+        similique laboriosam, molestias nisi aliquam recusandae excepturi sint
+        voluptatem cupiditate, culpa aspernatur non ex quas harum expedita nulla
+        neque ipsum debitis. Tempore veritatis possimus distinctio adipisci
+        fugit, nisi reprehenderit porro, ipsum officia veniam totam iusto
+        explicabo animi voluptas cumque et cupiditate ex sapiente non id minima
+        eos dolorem aspernatur. Porro minima cupiditate, iusto nemo minus unde
+        exercitationem officiis, sapiente veniam quos quasi inventore! Eaque
+        ratione accusantium laborum at vero nemo, deserunt dolorem quas velit.
+        Nostrum nihil modi pariatur laudantium. Eligendi minus cum veniam nam,
+        alias illo minima repellat. Ex vel in nisi et corrupti. Natus quia odit
+        ratione saepe libero id consequatur, quos voluptatum sint tempora
+        temporibus, quas, hic exercitationem repudiandae soluta. Unde vel esse
+        perferendis. Deleniti, incidunt. Maiores illum sint quibusdam, iste
+        molestias illo! Doloremque incidunt soluta blanditiis assumenda
+        distinctio repellendus error ratione, quae, amet odio atque minus
+        consequatur eius, corporis harum rerum fugit iure voluptatum excepturi
+        iusto exercitationem dolor velit deserunt tenetur. In, fuga! Maiores
+        dolores magni aspernatur optio recusandae amet odit, quia vel a
+        exercitationem ea, ut repellendus debitis, expedita at consequatur?
+        Alias aperiam officiis magnam ab dolores, ea, beatae fuga dolore unde
+        aut fugit quidem, nam exercitationem quam repellat dicta odit commodi.
+        Libero tempore sunt iste asperiores quod sit, illum tempora blanditiis
+        temporibus, in sequi necessitatibus fugiat corrupti rerum dolorem dolore
+        quidem debitis excepturi! Quo, voluptatem eligendi nulla consequatur
+        cupiditate, assumenda sint esse distinctio est excepturi totam dicta
+        placeat quisquam eaque quidem voluptates tempore eveniet incidunt
+        obcaecati, error qui! Accusantium, exercitationem, et perspiciatis nam
+        fugit aspernatur quas voluptatum, culpa inventore sunt error vel non
+        dignissimos consequuntur harum in. Quos nemo facilis ducimus, cupiditate
+        architecto expedita animi recusandae, inventore perferendis earum soluta
+        omnis. Dolorem ullam vero voluptatem amet excepturi corrupti, obcaecati
+        aperiam sed quo vel.
+      </p>
+    </div>
+  );
+}

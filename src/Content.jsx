1971d646ae8974292b4a3b2c6be55de32b18ecf3
Eyosyas Fetene <eyosifetene98@gmail.com>
2024-07-03 18:50:12 +0300

    Chapter 6 finished for dave gray course

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 
 const Content = () => {
-  const handleNameChange = () => {
-    const [name, setName] = useState("someone");
-    console.log(name);
+  const [name, setName] = useState("Dave");
+  const [count, setCount] = useState(0);
 
+  const handleNameChange = () => {
     const names = ["Bob", "Kevin", "Dave"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("You Clicked It!");
+    setCount(count + 1);
+    console.log(count);
   };
   const handleClick2 = (name) => {
-    console.log(`${name} Clicked It!`);
+    console.log(count);
   };
   const handleClick3 = (e) => {
     console.log(e.target);
   };
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {}!</p>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => handleClick2("Bob")}>Click Me</button>
-      <button onClick={(e) => handleClick3(e)}>Click Me</button>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <p>Count: {count}</p>
+      <button onClick={handleNameChange}>Change Me</button>
+      <button onClick={handleClick}>Add To Count </button>
+      <button onClick={handleClick2}>Show Count</button>
     </main>
   );
 };

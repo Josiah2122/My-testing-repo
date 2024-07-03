573f17065a5c5c734e5112fe17b3f2f87d2934d1
Eyosyas Fetene <eyosifetene98@gmail.com>
2024-07-03 19:20:00 +0300

    check box complete for grocery lists

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,32 +1,50 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  const [name, setName] = useState("Dave");
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Coca Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "One cup of granola",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "One cup of oatmeal",
+    },
+  ]);
 
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
   };
 
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
-  const handleClick2 = (name) => {
-    console.log(count);
-  };
-  const handleClick3 = (e) => {
-    console.log(e.target);
-  };
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}!</p>
-      <p>Count: {count}</p>
-      <button onClick={handleNameChange}>Change Me</button>
-      <button onClick={handleClick}>Add To Count </button>
-      <button onClick={handleClick2}>Show Count</button>
+      <ul>
+        {items?.map((item) => {
+          return (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                name="item"
+                checked={item.checked}
+                onChange={() => handleCheck(item.id)}
+              />
+              <label>{item.item}</label>
+              <FaTrashAlt role="button" tabIndex="0" />
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Groceries List</h1>
     </header>
   );
 }

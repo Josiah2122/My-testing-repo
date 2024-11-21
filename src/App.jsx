b3393b7995ac835a763ddf3cdbb7030b01d8f032
Eyosyas Fetene <eyosifetene98@gmail.com>
2024-11-21 08:56:11 +0300

    CRUD completed for the rest api

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Footer from "./Footer";
 import Header from "./Header";
 import AddItems from "./AddItems";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 function App() {
   const API_URL = "http://localhost:3500/items";
+
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   //You cannot use the async keyword in useEffect so you will see the old way of fetching inside useEffect
   useEffect(() => {
@@ -17,40 +21,64 @@ function App() {
     const fetchItems = async () => {
       try {
         const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive expected data");
         const listItems = await response.json();
-        console.log(listItems, "listItems");
         setItems(listItems);
+        setFetchError(null);
       } catch (err) {
-        console.log(err.stack);
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    (async () => await fetchItems())();
+    setTimeout(() => fetchItems(), 2000);
   }, []);
-  console.log(items, "items");
 
-  const setAndSaveItems = (items) => {
-    setItems(items);
-    localStorage.setItem("ShoppingList", JSON.stringify(items));
-  };
-
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myNewItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const deleteOptions = { method: "DELETE" };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(null);
   };
 
   const handleSubmit = (e) => {
@@ -69,13 +97,21 @@ function App() {
           handleSubmit={handleSubmit}
         />
         <SearchItem search={search} setSearch={setSearch} />
-        <Content
-          items={items?.filter((item) =>
-            item?.item?.toLowerCase()?.includes(search?.toLowerCase())
+        <main>
+          {fetchError ? (
+            <p style={{ color: "red" }}>{`Error : ${fetchError}`}</p>
+          ) : isLoading ? (
+            <p>Loading Items...</p>
+          ) : (
+            <Content
+              items={items?.filter((item) =>
+                item?.item?.toLowerCase()?.includes(search?.toLowerCase())
+              )}
+              handleCheck={handleCheck}
+              handleDelete={handleDelete}
+            />
           )}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
+        </main>
         <Footer length={items.length} />
       </div>
     </>
